8a24990f33e0c7eaf4b6d72d1b5c050aaf6b4a99
Mia Seo <tjwlfif@gmail.com>
2023-03-31 15:50:51 +0900

    Add: login 페이지 레이아웃 구현완료

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import './Signin.scss';
 
 const Signin = () => {
-  return <div>signin</div>;
+  return (
+    <div className="signin">
+      <h1>SignIn</h1>
+      <form className="signinForm">
+        <div className="inputBox">
+          <label htmlFor="email">email</label>
+          <input id="email" type="text" data-testid="email-input" />
+        </div>
+        <div className="inputBox">
+          <label htmlFor="password">password</label>
+          <input id="password" type="password" data-testid="password-input" />
+        </div>
+        <button data-testid="signin-button">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signin;
