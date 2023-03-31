420735ea692ee4e5bd609bf148227736e2724cc6
Mia Seo <tjwlfif@gmail.com>
2023-03-31 16:12:10 +0900

    Add: 투두리스트 레이아웃 구현 완료

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 import './Todo.scss';
 
 const Todo = () => {
-  return <div>todo</div>;
+  return (
+    <div className="todo">
+      <h1>To-Do List</h1>
+      <div className="todoContainer">
+        <div className="createTodo">
+          <input type="text" />
+          <button>create</button>
+        </div>
+        <div>
+          <li className="todoList">
+            <label>
+              <input type="checkbox" />
+              <span>TODO 1</span>
+            </label>
+            <button>edit</button>
+            <button>delete</button>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
